7781c820ae5757d92f9fb340e4d0ac9feca93e87
Yoseph Alabdulwahab <10735559+yosephsa@users.noreply.github.com>
2023-07-03 00:28:43 -0500

    feat (about): add basic about info

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <br>
+    <p>
+      This is a lightweight and easy to use flashcard studying site made by 
+      <a href="https://www.linkedin.com/in/yosephsa/">Yoseph Alabdulwahab.</a></p>
+    <br>
+    <p>
+      The goal of this project is to provide a studying platform that's free and as easy to use for
+      my friends. It shouldn't cost you money to flip through flashcards on the bus or in the hall
+      when you're study material is at home. By adding three sided cards and spaced repitition
+      we have a system that's guarenteed to make you ace that test.
+    </p>
+    <br>
+    <p>
+      The second goal is gain some experience in Vue3 and implementing a project methodically. 
+      Leveraging all my skill sets and making a clean open source project with common sense commits,
+      scoped tasks, competitor research and taking notes scientific literature on the topic. 
+      For any questions reach out on <a href="https://www.linkedin.com/in/yosephsa/">linkedIn</a>.
+    </p>
+    
   </div>
 </template>
 
@@ -9,7 +27,9 @@
   .about {
     min-height: 100vh;
     display: flex;
+    flex-direction: column;
     align-items: center;
+    justify-content: center;
   }
 }
 </style>
