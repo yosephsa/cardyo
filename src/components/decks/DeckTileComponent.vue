9c72ea01761c2b02419eaafeaba6418651ceaec1
Yoseph Alabdulwahab <10735559+yosephsa@users.noreply.github.com>
2023-07-02 19:17:41 -0500

    feat: implement delete & overhaul decks and mutability

--- src/components/decks/DeckTileComponent.vue
+++ src/components/decks/DeckTileComponent.vue
@@ -3,52 +3,61 @@ import { type Deck } from '@/models/decks.model';
 import type { PropType } from 'vue/dist/vue.js';
 import ModifyDeckComponent from './ModifyDeckComponent.vue'
 import ViewDeckComponent from './ViewDeckComponent.vue';
+import ContextMenu from '@imengyu/vue3-context-menu'
 
+export default {
+    props: {
+        deck: Object as PropType<Deck>,
+    },
+    data() {
+        return {
+            viewDialog: false,
+            editDialog: false
+        }
+    },
+    emits: ['update:deck'],
+    methods: {
+        onContextMenu(e : MouseEvent) {
+            e.preventDefault();
 
-    export default {
-        props: {
-            deck: Object as PropType<Deck>,
-            editMode: Boolean,
-            deleteMode: Boolean
-        },
-        data() {
-            return {
-                deck: this.$props.deck,
-                dialog: false,
-                showAnswer: false
-            }
-        },
-        emits: ['update:editMode'],
-        computed: {
-            isDeleteMode(): Boolean {
-                return !this.editMode && this.deleteMode;
-            },
-            editMode: {
-                    get(): Boolean {
-                        return this.editMode
-                    },
-                    set(editMode: Boolean) {
-                        this.$emit('update:editMode', editMode)
+            ContextMenu.showContextMenu({x: e.x, y: e.y, items: [
+                { 
+                    label: "View", 
+                    onClick: () => { 
+                        this.viewDialog = true; 
                     }
-                }
-        },
-        components: { ModifyDeckComponent, ViewDeckComponent }
-    }
+                },
+                { 
+                    label: "Edit", 
+                    onClick: () => { 
+                        this.editDialog = true;
+                    }
+                },
+                { 
+                    label: "Delete", 
+                    onClick: () => { 
+                        this.$emit('update:deck', null);
+                    }
+                },
+            ]
+            }); 
+        }
+    },
+    components: { ModifyDeckComponent, ViewDeckComponent }
+}
 </script>
 
 <template>
-
 <v-btn v-if="deck"
-      color="primary"
-      :variant="editMode ? 'outlined' : 'flat'"
-      @Click="dialog = true"
+    color="primary"
+    variant="outlined"
+    @Click="viewDialog = true"
+    @contextmenu="onContextMenu($event)"
     >
-    <v-icon v-if="editMode" icon="mdi-pencil"></v-icon>
-    <v-icon v-if="isDeleteMode" icon="mdi-pencil"></v-icon>
-    <span>&nbsp;{{ deck.name }} </span>
+    <span>&nbsp;{{ deck?.name }} </span>
 
-    <ViewDeckComponent v-if="!editMode" :deck="deck" v-model:dialog="dialog"></ViewDeckComponent>
-    <ModifyDeckComponent v-if="editMode" v-model:deck="deck" v-model:dialog="dialog"></ModifyDeckComponent>
+    <ViewDeckComponent v-if="viewDialog" :deck="deck" v-model="viewDialog"></ViewDeckComponent>
+    <ModifyDeckComponent v-if="editDialog" @update:deck="tempDeck => $emit('update:deck', tempDeck)" :deck="deck" v-model="editDialog"></ModifyDeckComponent>
 </v-btn>
 
 </template>
